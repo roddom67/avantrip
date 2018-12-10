7f1bddf316b8e077f38245a4b0d8207d75511891
roddom67 <roddom67@gmail.com>
2018-12-10 09:20:25 -0300

    avantrip

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="cache-control" content="no-cache" />
+	<meta http-equiv="expires" content="0" />
+	<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="pragma-directive: no-cache">
+	<meta http-equiv="cache-directive: no-cache">
+	
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+	
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+   <meta name="mobile-web-app-capable" content="yes">
+   
+	<link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,200,300,400,600,700,900" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css" >
+	<link rel="stylesheet" href="css/style.css" >
+	
+	<title>New York | Avantrip</title>
+	<meta name="description" content="Escapate a Nueva York con Avantrip y recorré la Gran Manzana. Descubrí los Hoteles y Vuelos a Nueva York con Avantrip." />
+	<meta name="keywords" content="neuva york, avantrip, gran manzana, hoteles, vuelos, escapate">
+	
+</head>
+
+
+<!doctype html>
+<html lang="es">
+   <head prefix="og: http://ogp.me/ns#">
+      <script type="text/javascript">var loc=window.location.href;-1!=loc.indexOf("/site/static/")&&document.write("<base href='/site/static/' />")</script>
+      <base href="/">
+      <meta charset="UTF-8"/>
+      <meta name="robots" content="index,follow"/>
+      <meta http-equiv="cache-control" content="no-cache"/>
+      <meta http-equiv="expires" content="0"/>
+      <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT"/>
+      <meta http-equiv="pragma" content="no-cache"/>
+      <meta http-equiv="pragma-directive: no-cache"/>
+      <meta http-equiv="cache-directive: no-cache"/>
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+      <meta name="format-detection" content="telephone=no"/>
+      <title>Vuelos y Hoteles a New York | Avantrip</title>
+      <meta name="description" content="¿Buscás ofertas en Vuelos y Hoteles a Nueva York Llegaste al lugar indicado. En Avantrip podrás encontrar los mejores precios para reservar tu vuelo y hotel a Nueva York. No esperes más ¡es fácil, seguro y rápido! Escapate a Nueva York.">
+      <meta property="og:title" content="Vuelos y Hoteles a New York | Avantrip"/>
+      <meta property="og:description" content="¿Buscás ofertas en Vuelos y Hoteles a Nueva York Llegaste al lugar indicado. En Avantrip podrás encontrar los mejores precios para reservar tu vuelo y hotel a Nueva York. No esperes más ¡es fácil, seguro y rápido! Escapate a Nueva York."/>
+      
+      <link rel="shortcut icon" href="favicon.ico"/>
+      <link rel="stylesheet" href="css-react/normalize.css?v=1.0"/>
+      <link rel="stylesheet" href="css/bootstrap.min.css?v=1.0" >
+		<link rel="stylesheet" href="css/style.css?v=1.0" >
+   </head>
+   <body>
+      <div id="wrapper" class="staticPage flights-to hotels-to newYork">
+         <header id="header">
+         	<nav>
+         		<ul>
+         			<li class="empresaArea">
+         				<a href="/" title="Avantrip"><img src="" alt="Avantrip" title="Avantrip"></a>
+         			</li>
+         			<li class="menuArea">
+         				<ul>
+         					<li>
+         						<a href="" title="Vuelos">Vuelos</a>
+         					</li>
+         					<li>
+         						<a href="" title="Hoteles">Hoteles</a>
+         					</li>
+         					<li>
+         						<a href="" title="Paquetes">Paquetes</a>
+         					</li>
+         					<li>
+         						<a href="" title="Autos">Autos</a>
+         					</li>
+         					<li>
+         						<a href="" title="más">más</a>
+         						<ul>
+         							<li>
+         								<a href="" title="Cruceros">Cruceros</a>
+         							</li>
+         							<li>
+         								<a href="" title="Disney">Disney</a>
+         							</li>
+         						</ul>
+         					</li>
+         				</ul>
+         			</li>
+         			<li class="usuarioArea">
+         				<p>Bienvenido, <strong>Alejandro Altamirano</strong><a href="" title="Usuario"><i>></i></a></p>
+         				<ul>
+         					<li>
+         						<a href="" title="Mi cuenta">Mi cuenta</a>
+         					</li>
+         					<li>
+         						<a href="" title="Log out">Log Out</a>
+         					</li>
+         				</ul>
+         			</li>
+         		</ul>
+         	</nav>
+         </header>
+         <main id="landscape">
+            <div class="imagen-titulo"><img src="images/vuelos-a-nueva-york.jpg" alt="Vuelos y Hoteles a Nueva York" title="Vuelos y Hoteles a Nueva York"/></div>
+            <aside class="breadcrumb">
+               <div class="contentLandscape">
+                  <ul>
+                     <li><a href="/" title="Home"><span>Home</span></a></li>
+                     <li><a href="/destinos.html" title="Destinos"><span>Destinos</span></a></li>
+                     <li><a href="/destinos/estados-unidos.html" title="Estados Unidos"><span>Estados Unidos</span></a></li>
+                     <li><a href="/destinos/estados-unidos/nueva-york.html" title="Nueva York"><strong>Nueva York</strong></a></li>
+                  </ul>
+               </div>
+            </aside>
+            <section class="contentLandscape">
+               <header class="info">
+                  <h1 class="txtHidden">Vuelos y Hoteles a Nueva York</h1>
+                  <h2><span>Escapate a</span> Nueva York</h2>
+               </header>
+               <section class="search">
+                  <article id="flight" class="active">
+                     <h3><span class="hidden">Buscar</span>Vuelos</h3>
+                     <form action="" class="goBack">
+                        <fieldset>
+                           <ul class="flightType">
+                              <li class="hidden"><input type="radio" id="goBack" name="typeFlight" value="Return" checked="checked"/> <label for="goBack">Ida y Vuelta</label></li>
+                              <li class="hidden"><input type="radio" id="go" name="typeFlight" value="OneWay"/> <label for="go">Solo ida</label></li>
+                              <li class="hidden"><input type="radio" id="multi" name="typeFlight" value="Other"/> <label for="multi">Multidestino</label></li>
+                              <li><input name="rest" type="hidden" id="rest" value="true"/></li>
+                           </ul>
+                        </fieldset>
+                        <fieldset class="destinyWrapper">
+                           <ul>
+                              <li class="destiny">
+                                 <ul>
+                                    <li>
+                                    	<label for="origenText">Origen</label> 
+                                    	<input name="origenText" id="origenText" type="text" class="autoCompleteFlag originPlace" value="Buenos Aires (EZE)"/> 
+                                    </li>
+                                    <li>
+                                    	<label for="destinoText">Destino</label> 
+                                    	<input id="destinoText" name="destinoText" type="text" class="autoCompleteFlag destinyPlace" value="Nueva York (JFK)" readonly="readonly"/> 
+												</li>
+                                    <li>
+                                    	<label for="fechaIda">Fechas</label> 
+                                    	<input class="datePickerFlag originDate" type="text" readonly="readonly"/> 
+                                    </li>
+                                    <li>
+                                    	<label for="pasajeros" class="passengers">Pasajeros y Clase</label> 
+                                    	<input type="text" class="passengerText" readonly="readonly"/> 
+                                    	<input name="fechaVuelta" id="fechaVuelta" class="destinyDateHidden" type="hidden"/>
+                                    </li>
+                                 </ul>
+                              </li>
+                              <li>
+                            		<input name="categoryGA" value="vuelos" type="hidden"/> 
+                           		<input type="submit" name="btnResultadoBusqueda" id="btnResultadoBusqueda" value="Buscar"/>
+                             </li>
+                           </ul>
+                        </fieldset>
+                     </form>
+                  </article>
+               </section>
+            </section>
+         </main>
+         <section class="preciosPromedio">
+         	<div class="container">
+         		<div class="preciosPromedioArea">
+         			<div class="fechasArea">
+         				<ul>
+         					<li>
+         						flecha
+         					</li>
+         					<li>
+         						<h4>Abril</h4>
+         						<small>desde AR$ 15.100</small>
+         					</li>
+         				</ul>
+         			</div>
+         			<div class="dataPrecios">
+         				Grafico
+         			</div>
+         		</div>
+         	</div>
+         </section>
+         <section class="textArea">
+         	<div class="container">
+         		<h2>La Gran Manzana</h2>
+         		<p>Wheather its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation, It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list before you leave home so you're ready for any type of weather. If you're prepared then all you need to do is focus on having a fun and relaxing time.</p>
+         	</div>
+         </section>
+         
+         <section class="ofertasLanding" id="landingPage">
+            <div class="container offerContent">
+               <h2>Hoteles en Nueva York</h2>
+               <div id="destacadas" class="ofertasContent row">
+                  <ul class="offers ofertaData row">
+                  	<li>
+                  		<a href="">
+                  			<div>
+                  				<span class="spanImg">
+                  					<img src="" alt="" title="">
+                  				</span>
+                  				<div class="textAreaOferta">
+                  					<p>Hotel en Nueva York</p>
+                  					<h2>Yotel</h2>
+                  					<span class="stars star4"></span>
+                  					<div class="priceArea">
+                  						<small>Desde</small>
+                  						<p>AR$ 4.850</p>
+                  						<small class="before">Antes <span>AR$ 7.462</span></small>
+                  					</div>
+                  					<small>Habitación por noche</small>
+                  					<h5>Ver Hotel</h5>
+                  				</div>
+                  			</div>
+                  		</a>
+                  	</li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+         <footer id="footer">
+         	
+         </footer>
+      </div>
+      <script type="text/javascript" src="js/jquery-1.10.2.min.js?v=1.0.1"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js?v=1.0"></script>
+      <!--[if (lte IE 9)]>
+      <script type="text/javascript" src="js/mediaQueryPolyFill.js"></script>
+      <script type="text/javascript" src="js/adapt.min.js"></script>
+      <![endif]-->
+   </body>
+</html>
+
